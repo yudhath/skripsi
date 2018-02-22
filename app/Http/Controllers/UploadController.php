64949608c840960c2c27d90e62d27916245ae426
Yudha <yudha@Yudhas-MacBook-Air.local>
2018-02-22 19:34:16 +0700

    commit max execution time

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -11,6 +11,7 @@ use \Input as Input;
 class UploadController extends Controller
 {
     public function upload(Request $request){
+    	ini_set('max_execution_time', 300);
 
     	//upload image
     	if(Input::hasFile('file')){
